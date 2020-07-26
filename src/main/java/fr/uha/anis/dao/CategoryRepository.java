8f3377796fe09184bee0456a39e6da329f9039ca
Anis KCHAOU <kchaouanis25@gmail.com>
2020-07-26 20:16:21 +0200

    Update CategoryRepository.java

--- src/main/java/fr/uha/anis/dao/CategoryRepository.java
+++ src/main/java/fr/uha/anis/dao/CategoryRepository.java
@@ -1,11 +1,12 @@
 package fr.uha.anis.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
 import fr.uha.anis.entities.Category;
 
-@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
+@CrossOrigin("*")
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
